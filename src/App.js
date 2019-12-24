6cd1758ee45f7bc0998bd0a7f64f94a6adb016bf
Ahmed Elbohoty <a.ramzielbohoty@gmail.com>
2019-12-24 14:14:31 +0200

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Todo from "./components/Todo";
+import Timer from "./components/Timer";
+
+import "./style/base.css";
+import "./style/layout.css";
+import "./style/status.css";
+import "./style/modules/index.css";
+
+export const AppContext = React.createContext();
 
 function App() {
+  const [todo, setTodo] = useState("");
+  const [showTimer, setShowTimer] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppContext.Provider value={{ todo, setTodo, setShowTimer }}>
+      <div id="l-app">
+        {!showTimer && <Todo />}
+
+        {showTimer && <Timer />}
+      </div>
+    </AppContext.Provider>
   );
 }
 
